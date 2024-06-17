50841cea1b1df9fa3a15e306e115ecfa049b48ec
akshath3001 <99238239+akshath3001@users.noreply.github.com>
2024-06-17 15:12:37 +0530

    Update export.py

--- export.py
+++ export.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 import pickle
 
 from dataset.augmentation import get_transform
