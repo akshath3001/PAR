006d9978202ca6eabd30dc8b9442e359ac8c9800
akshath3001 <99238239+akshath3001@users.noreply.github.com>
2024-06-17 15:12:01 +0530

    Add files via upload

---
+++ export.py
@@ -0,0 +1,118 @@
+import argparse
+import json
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+import pickle
+
+from dataset.augmentation import get_transform
+from dataset.multi_label.coco import COCO14
+from metrics.pedestrian_metrics import get_pedestrian_metrics
+from models.model_factory import build_backbone, build_classifier
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from configs import cfg, update_config
+from dataset.pedes_attr.pedes import PedesAttr
+from metrics.ml_metrics import get_map_metrics, get_multilabel_metrics
+from models.base_block import FeatClassifier
+# from models.model_factory import model_dict, classifier_dict
+
+from tools.function import get_model_log_path, get_reload_weight
+from tools.utils import set_seed, str2bool, time_str
+from losses import bceloss, scaledbceloss
+
+set_seed(605)
+
+
+def main(cfg, args):
+    exp_dir = os.path.join('exp_result', cfg.DATASET.NAME)
+    model_dir, log_dir = get_model_log_path(exp_dir, cfg.NAME)
+
+    train_tsfm, valid_tsfm = get_transform(cfg)
+    print(valid_tsfm)
+
+    if cfg.DATASET.TYPE == 'multi_label':
+        train_set = COCO14(cfg=cfg, split=cfg.DATASET.TRAIN_SPLIT, transform=train_tsfm,
+                           target_transform=cfg.DATASET.TARGETTRANSFORM)
+
+        valid_set = COCO14(cfg=cfg, split=cfg.DATASET.VAL_SPLIT, transform=valid_tsfm,
+                           target_transform=cfg.DATASET.TARGETTRANSFORM)
+    else:
+        train_set = PedesAttr(cfg=cfg, split=cfg.DATASET.TRAIN_SPLIT, transform=valid_tsfm,
+                              target_transform=cfg.DATASET.TARGETTRANSFORM)
+        valid_set = PedesAttr(cfg=cfg, split=cfg.DATASET.VAL_SPLIT, transform=valid_tsfm,
+                              target_transform=cfg.DATASET.TARGETTRANSFORM)
+
+
+    train_loader = DataLoader(
+        dataset=train_set,
+        batch_size=cfg.TRAIN.BATCH_SIZE,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True,
+    )
+
+    valid_loader = DataLoader(
+        dataset=valid_set,
+        batch_size=cfg.TRAIN.BATCH_SIZE,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True,
+    )
+
+    print(f'{cfg.DATASET.TRAIN_SPLIT} set: {len(train_loader.dataset)}, '
+          f'{cfg.DATASET.TEST_SPLIT} set: {len(valid_loader.dataset)}, '
+          f'attr_num : {train_set.attr_num}')
+
+    backbone, c_output = build_backbone(cfg.BACKBONE.TYPE, cfg.BACKBONE.MULTISCALE)
+
+
+    classifier = build_classifier(cfg.CLASSIFIER.NAME)(
+        nattr=train_set.attr_num,
+        c_in=c_output,
+        bn=cfg.CLASSIFIER.BN,
+        pool=cfg.CLASSIFIER.POOLING,
+        scale =cfg.CLASSIFIER.SCALE
+    )
+
+    model = FeatClassifier(backbone, classifier)
+
+    if torch.cuda.is_available():
+        model = torch.nn.DataParallel(model).cuda()
+
+    model = get_reload_weight(model_dir, model, pth=cfg.RELOAD.PTH)
+
+    model.eval()
+    # dummy_imp=
+    torch.onnx.export(model,
+                      dummy_input,"solider_par.onnx",
+                      export_params=True,
+                      dynamix_axes={'input': {0:cfg.TRAIN.BATCH_SIZE},
+                                    'output': {0:cfg.TRAIN.BATCH_SIZE}})
+def argument_parser():
+    parser = argparse.ArgumentParser(description="attribute recognition",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        "--cfg", help="decide which cfg to use", type=str,
+        default="./configs/pa100k.yaml",
+
+    )
+
+    parser.add_argument("--debug", type=str2bool, default="true")
+    parser.add_argument('--local_rank', help='node rank for distributed training', default=0,
+                        type=int)
+    parser.add_argument('--dist_bn', type=str, default='',
+                        help='Distribute BatchNorm stats between nodes after each epoch ("broadcast", "reduce", or "")')
+
+    args = parser.parse_args()
+
+    return args
+if __name__ == '__main__':
+    args = argument_parser()
+    update_config(cfg, args)
+
+    main(cfg, args)
